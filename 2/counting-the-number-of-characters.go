d4af7748853840b027710d0ee843beb423bf39c7
Ville Viklund <ville.viklund@geniem.com>
2023-09-03 22:44:13 +0300

    Formatting

--- 2/counting-the-number-of-characters.go
+++ 2/counting-the-number-of-characters.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("What is the input string?")
-    var input string
-    fmt.Scanln(&input)
+	fmt.Println("What is the input string?")
+	var input string
+	fmt.Scanln(&input)
 
-    fmt.Printf("%s has %d characters. \n", input, len(input))
+	fmt.Printf("%s has %d characters. \n", input, len(input))
 }
